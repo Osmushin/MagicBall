520b8dd6f0ddefebe6c34b7bbddc529e427cd52a
Osmushin <toxa_os@mail.ru>
2023-01-11 12:45:37 +0300

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from random import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+answers = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 'Определенно да', 'Можешь быть в этом уверен',
+           'Мне кажется - да', 'Вероятнее всего', 'Хорошие перспективы', 'Знаки говорят - да', 'Да',
+           'Пока не ясно, пробуй снова', 'Спроси позже', 'Лучше не рассказывать', 'Сейчас нельзя предсказать',
+           'Сконцентрируйся и спроси опять', 'Даже не думай', 'Мой ответ - нет', 'По моим данным - нет',
+           'Перспективы не очень хорошие', 'Весьма сомнительно']
+
+print('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.')
+print('Как вас зовут?')
+name = input()
+print('Приветствую тебя,', name, '!')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def magic_ball():
+    print('Какой вопрос вы хотите задать?')
+    input()
+    print(choice(answers))
+    print('У вас есть еще вопросы? Если хотите задать вопрос напишите "да", если вопросов не осталось напишите "нет"')
+    if input().lower() == 'да':
+        return True
+    else:
+        print('Возвращайся если возникнут вопросы!')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while magic_ball() == True:
+    magic_ball()
